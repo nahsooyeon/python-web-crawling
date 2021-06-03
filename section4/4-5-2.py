d7c20efdeaab616687055313d4fa8ae71d96fd4d
nahsooyeon <nahsooyeon@gmail.com>
2021-06-03 17:34:47 +0900

    파이썬 Pandas 사용하기(1) - CSV 읽기, 쓰기

--- section4/4-5-2.py
+++ section4/4-5-2.py
@@ -5,7 +5,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 #특정 문자 구분
-df2 = pd.read_csv('c:/section4/csv_s2.csv',sep=';', skiprows=[0], header=None, names=["First name", "Test1", "Test2", "Test3", "Final", "Grade"])
+df2 = pd.read_csv('/Users/devpomme/Desktop/development/web-crawling/python_create_app_1/section4/csv_s2.csv',sep=';', skiprows=[0], header=None, names=["First name", "Test1", "Test2", "Test3", "Final", "Grade"])
 df2['Grade'] = df2['Grade'].str.replace('"', ' ')
 df2['Avg'] = df2[['Test1','Test2','Test3','Final']].mean(axis=1)
 df2['Sum'] = df2[['Test1','Test2','Test3','Final']].sum(axis=1)
@@ -13,5 +13,5 @@ df2['Sum'] = df2[['Test1','Test2','Test3','Final']].sum(axis=1)
 print(df2)
 
 #CSV 쓰기
-#df2.to_csv('c:/section4/result.csv')
-df2.to_csv('c:/section4/result.csv',index=False)
+# df2.to_csv('/Users/devpomme/Desktop/development/web-crawling/python_create_app_1/section4/result.csv')
+df2.to_csv('/Users/devpomme/Desktop/development/web-crawling/python_create_app_1/section4/result.csv',index=False)
--- section4/4-5-3.py
+++ section4/4-5-3.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 #랜덤으로 DataFrame 생성
-df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
+df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns= ['ONE', 'TWO', 'THREE', 'FOUR'])
 #df = pd.DataFrame(np.random.randn(100, 4), columns=list('ABCD'))
-print(df)
+df2 = df.rename(index=lambda x : x+1)
+
 
 #CSV 쓰기
-df.to_csv('c:/section4/result.csv',index=False)
+df2.to_csv('/Users/devpomme/Desktop/development/web-crawling/python_create_app_1/section4/result.csv',index=False)
 #df.to_csv('c:/section4/result.csv',index=False,header=False)
+  
