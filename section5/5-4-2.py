a6e86424a6ce401b63218b3ff952abb3a65ce778
nahsooyeon <nahsooyeon@gmail.com>
2021-06-07 17:42:45 +0900

    파이썬 MySQL 연동하기 - 데이터 추가/조회/삭제

--- section5/5-4-2.py
+++ section5/5-4-2.py
@@ -2,18 +2,23 @@ import pymysql
 import simplejson as json
 
 # MySQL Connection 연결
-conn = pymysql.connect(host='localhost', user='python', password='1111!',
-                       db='python_app1', charset='utf8') #autocommit=True
+conn = pymysql.connect(host='localhost', user='root', password='password',
+                       db='python_app1', charset='utf8')  #autocommit=True
 
 try:
     with conn.cursor() as c: #딕셔너리 반환 : conn.cursor(pymysql.cursors.DictCursor)
         c.execute("SELECT * FROM users")
+
+        print("====================로우 선====================")
+
         #1개 로우 선택
         print(c.fetchone())
         #지정 로우 선택
         print(c.fetchmany(3))
         #전체 로우 선택
         print(c.fetchall())
+
+        print("====================순회====================")
 
         #순회1
         c.execute("SELECT * FROM users ORDER BY id ASC")
@@ -26,22 +31,29 @@ try:
         for row in c.fetchall():
             print('usage2 >',row)
 
+        print("====================조건조회====================")
         #조건 조회1
         param1 = (1,)
         c.execute('SELECT * FROM users WHERE id=%s', param1)
         #print('param1',c.fetchone())
         print('param1',c.fetchall())
 
-        #조건 조회2
+        #조건 조회2 - python formatting: %s, %f, %d, %o..
         param2 = 1
         c.execute("SELECT * FROM users WHERE id='%s'" % param2)
         #print('param2',c.fetchone())
         print('param2',c.fetchall())
 
-        #조건 조회3
+        #조건 조회3 - 조건 두 개
+
+        param3 = (4,5)
+        c.execute("SELECT * FROM users WHERE id IN(%s, %s)", param3)
+        print('param3', c.fetchall())
+
+
         param4 = (1,4)
         c.execute('SELECT * FROM users WHERE id IN(%s,%s)', param4)
-        print('param3',c.fetchall())
+        print('param4',c.fetchall())
 
         #조건 조회4
         c.execute("SELECT * FROM users WHERE id In('%d','%d')" % (1, 4)) # python formatting %s, %d, %f...
--- section5/5-4-3.py
+++ section5/5-4-3.py
@@ -2,7 +2,7 @@ import pymysql
 import simplejson as json
 
 # MySQL Connection 연결
-conn = pymysql.connect(host='localhost', user='python', password='1111!',
+conn = pymysql.connect(host='localhost', user='root', password='password',
                        db='python_app1', charset='utf8') #autocommit=True
 
 try:
@@ -10,27 +10,28 @@ try:
         #데이터 수정1
         c.execute("UPDATE users SET username = %s WHERE id = %s", ('niceman', 1))
         #데이터 수정2
-        c.execute("UPDATE users SET username = '%s' WHERE id = '%d'" % ('cuteboy', 5))
+        c.execute("UPDATE users SET username = '%s' WHERE id = '%d'" % ('goodboy', 5))
+
 
         #중간데이터 확인1
         c.execute("SELECT * FROM users ORDER BY id DESC")
         for row in c.fetchall():
             print('check1 >',row)
-
+    #
         #데이터 삭제1
-        c.execute("DELETE FROM users WHERE id = %s", (7,))
-
-        #데이터 삭제2
-        c.execute("DELETE FROM users WHERE id = '%d'" % 9)
-
-        #중간데이터 확인2
+        c.execute("DELETE FROM users WHERE id = %s", (1,))
+    #
+    #     #데이터 삭제2
+        c.execute("DELETE FROM users WHERE id = '%s'" %(5,))
+    #
+    #     #중간데이터 확인2
         c.execute("SELECT * FROM users ORDER BY id DESC")
         for row in c.fetchall():
             print('check2 >',row)
-
-        #로우 카운트
-        print('rowCount : ',c.execute("SELECT * FROM users ORDER BY id DESC"))
-
-    conn.commit()
+    #
+    #     #로우 카운트
+    #     print('rowCount : ',c.execute("SELECT * FROM users ORDER BY id DESC"))
+    #
+    # conn.commit()
 finally:
     conn.close()
